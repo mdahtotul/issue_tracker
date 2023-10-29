6de9517ef217f87cc75c61f41a3640b6b1651d10
MD ARIFUL HASAN <77568259+mdahtotul@users.noreply.github.com>
2023-10-29 15:36:11 +0600

    handled delete errors

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -12,22 +12,58 @@ export async function PATCH(
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
 
-  const issue = await prisma.issue.findUnique({
-    where: {
-      id: +params.id,
-    },
-  });
+  try {
+    const issue = await prisma.issue.findUnique({
+      where: {
+        id: +params.id,
+      },
+    });
 
-  if (!issue)
-    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+    if (!issue)
+      return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
 
-  const updatedIssue = await prisma.issue.update({
-    where: { id: issue?.id },
-    data: {
-      title: body.title,
-      description: body.description,
-    },
-  });
+    const updatedIssue = await prisma.issue.update({
+      where: { id: issue?.id },
+      data: {
+        title: body.title,
+        description: body.description,
+      },
+    });
 
-  return NextResponse.json(updatedIssue, { status: 200 });
+    return NextResponse.json(updatedIssue, { status: 200 });
+  } catch (err: any) {
+    return NextResponse.json(
+      { error: err?.meta?.cause || "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (!params.id)
+    return NextResponse.json({ error: "Id is required" }, { status: 400 });
+
+  try {
+    const issue = await prisma.issue.delete({
+      where: {
+        id: +params.id,
+      },
+    });
+
+    if (!issue)
+      return NextResponse.json(
+        { error: "Item doesn't exist" },
+        { status: 404 }
+      );
+
+    return NextResponse.json(issue, { status: 200 });
+  } catch (err: any) {
+    return NextResponse.json(
+      { error: err?.meta?.cause || "Internal Server Error" },
+      { status: 400 }
+    );
+  }
 }
--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,30 +1,67 @@
 "use client";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function DeleteIssueButton({ issueId }: { issueId: number }) {
-  return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red"> Delete Issue</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Delete</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure you want to delete this issue? This action cannot be
-          undone
-        </AlertDialog.Description>
+  const router = useRouter();
+  const [error, setError] = useState<boolean>(false);
 
-        <Flex mt={"4"} gap={"3"}>
-          <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button color="red">Delete</Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+  const handleDelete = async (id: number) => {
+    try {
+      let url = `/api/issues/${id}`;
+      await axios.delete(url);
+      router.push("/issues");
+      router.refresh();
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  return (
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red"> Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Delete</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this issue? This action cannot be
+            undone
+          </AlertDialog.Description>
+
+          <Flex mt={"4"} gap={"3"}>
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" onClick={() => handleDelete(issueId)}>
+                Delete
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            This issue could not be deleted.
+          </AlertDialog.Description>
+          <Button
+            color="gray"
+            variant="soft"
+            mt={"4"}
+            onClick={() => setError(false)}
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 }
